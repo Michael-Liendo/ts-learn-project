e7b4b2c2b9b2438e580d6223ab00682bbf036b02
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-05 09:26:00 -0400

    other classes

---
+++ src/08_any.ts
@@ -0,0 +1,10 @@
+(() => {
+    //any= cualquier cosa puede ir ahí 
+    let myDynamicVar: any;
+
+    //podemos darle cualuqier valor
+    myDynamicVar = 100;
+    myDynamicVar = null;
+    myDynamicVar = {};
+    myDynamicVar ='';
+})();
